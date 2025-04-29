8f39fcee23e95a875115daa2ba3d3b802ad220a6
Bhuvan Shyam <137712197+BhuvanShyam@users.noreply.github.com>
2025-04-29 19:28:49 +0530

    Added post request

--- src/main/java/com/example/todoapispring/TodoController.java
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -1,20 +1,36 @@
 package com.example.todoapispring;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
 public class TodoController  {
-    private static List<Todo> todos
+    private static List<Todo> todosList;
 
     public TodoController(){
-        todos  = new ArrayList<>();
-        todos.add(new Todo(1,"Todo 1","Todo 1 is ongoing",1));
-        todos.add(new Todo(2,"Todo 2","Todo 2 is ongoing",2));
-        todos.add(new Todo(3,"Todo 3","Todo 3 is ongoing",3));
+        todosList  = new ArrayList<>();
+        todosList.add(new Todo(1,"Todo 1","Todo 1 is ongoing",1));
+        todosList.add(new Todo(2,"Todo 2","Todo 2 is ongoing",2));
+        todosList.add(new Todo(3,"Todo 3","Todo 3 is ongoing",3));
     }
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todosList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo  newTodo){
+        todosList.add(newTodo);
+        return  newTodo;
+    }
+
+
 
 
 
