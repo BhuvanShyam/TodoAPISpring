08e047c43cf7e384de9c9dbd156cacb8be0bb9ed
Bhuvan Shyam <137712197+BhuvanShyam@users.noreply.github.com>
2025-04-30 17:30:52 +0530

    Added post request

--- src/main/java/com/example/todoapispring/TodoController.java
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -1,14 +1,14 @@
 package com.example.todoapispring;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController  {
     private static List<Todo> todosList;
 
@@ -19,15 +19,27 @@ public class TodoController  {
         todosList.add(new Todo(3,"Todo 3","Todo 3 is ongoing",3));
     }
 
-    @GetMapping("/todos")
-    public List<Todo> getTodos(){
-        return todosList;
+    @GetMapping("/")
+    public ResponseEntity<List<Todo>> getTodos(){
+        return ResponseEntity.status(HttpStatus.OK).body(todosList);
     }
 
-    @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo  newTodo){
+    @PostMapping("/")
+//    @ResponseStatus(HttpStatus.CREATED)  for setting http status code correctly
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo  newTodo){
         todosList.add(newTodo);
-        return  newTodo;
+        return  ResponseEntity.status(HttpStatus.CREATED).body(newTodo); //another way to for setting http status code correctly
+    }
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable int todoId){
+        for(Todo todo:todosList){
+            if(todo.getId() ==  todoId){
+                return ResponseEntity.ok(todo);
+            }
+        }
+        //along with 404 status code, try to send a json {message: "Todo not found"}
+        return  ResponseEntity.notFound().build();
     }
 
 
